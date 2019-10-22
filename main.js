90580c245d65aa18a751315d84a3ab4497d0331f
UrtziFM <urtzi.arana13@gmail.com>
2019-10-22 11:51:39 +0200

    nuevos cambios

--- main.js
+++ main.js
@@ -3,3 +3,27 @@ setTimeout(function(){
     hello.innerHTML = '<strong>Urtzi Arana : Oh mama!!</strong>'
 }, 10000);
 
+//let formu = document.querySelector('form').addEventListener("submit")
+//console.log("formu")
+
+let arrowL = document.querySelector(".arrow.left");
+let arrowR = document.querySelector(".arrow.right");
+let indiceImg = 0;
+
+let imagenes = document.querySelectorAll("#slide>img");
+
+arrowR.addEventListener("click", ()=> {
+    if (indiceImg < imagenes.length -1 ){
+    imagenes[indiceImg].classList.replace("activa", "fuera")
+    indiceImg++;
+    imagenes[indiceImg].classList.add("activa")
+    }
+});
+
+arrowL.addEventListener("click", ()=> {
+    if (indiceImg > 0 ){
+        imagenes[indiceImg].classList.remove("activa")
+        indiceImg--;
+        imagenes[indiceImg].classList.replace("fuera", "activa")
+        }
+});
