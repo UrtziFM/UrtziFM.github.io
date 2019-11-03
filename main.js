e80d6d463c84a3eb053770e05c089d50f6730bb2
UrtziFM <urtzi.arana13@gmail.com>
2019-11-03 18:20:02 +0100

    cambios 03-11

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
+//Codigo para cambiar el texto del div a los 5 segundos
+
 var hello = document.querySelector('div.header')
 setTimeout(function(){
     hello.innerHTML = '<strong>Urtzi Arana : Oh mama!!</strong>'
 }, 5000);
 
-var nombre_visitante = prompt('Hello! What´s your name, please?');
-alert('Hola ' + nombre_visitante);
+//Codigo para pedir el nombre del usuario a la acceder a la web
 
-//document.querySelector('form').addEventListener("submit", function(evt){
-//    evt.preventDefault();
-//console.log(this.proposal.value);
-//})
+var nombre_visitante = prompt('Hello! What´s your name, please?');
+
+
+if (nombre_visitante === 'Urtzi'){
+    console.log("Urtzi you are a genius")
+    alert('Hello ' + nombre_visitante);
+}else if (nombre_visitante === 'Jobs'){
+    console.log("As Urtzi you both are genius")
+    alert('Hello ' + nombre_visitante);
+}else{
+    alert("As you are not Urtzi, sorry but you are not a genius")
+    alert('Hello ' + nombre_visitante);
+}
+// Codigo con bucle en caso de que el usuario de al intro sin introducir su nombre
+while (nombre_visitante == ''){
+    var nombre_visitante_intento = prompt('Please, tell me your name');
+    alert('Hello ' + nombre_visitante_intento);
+    break;
+}
+
+
+
+//Codigo para poder mover las fotos en la slide 
 
 let arrowL = document.querySelector('span.arrow.left');
 let arrowR = document.querySelector('span.arrow.right');
@@ -32,3 +52,5 @@ arrowL.addEventListener("click", ()=> {
         imagenes[indiceImg].classList.replace("fuera", "activa")
         }
 });
+
+//Obtener la información que mete el usuario en el formulario y mostrarlo en un console log
